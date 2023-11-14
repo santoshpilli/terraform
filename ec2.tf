f3b0fcd439f843bc91393ed852ba32cf6fd520f3
sanju <santosh.pilli@exceloid.com>
2023-11-14 18:43:14 +0530

    new

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,9 @@
-provider "aws" {
-access_key = "AKIA4J4J2RVWHDPXPN4Z"
-secret_key = "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"
-region = "ap-south-1"
+resource "aws_instance" "web" {
+  ami           = "ami-02e94b011299ef128"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
- 
-resource "aws_instance" "terraform_demo" {
-ami = "ami-02e94b011299ef128"
-instance_type = "t2.micro"
-}
+
